e6d29217589da6a1f25a614bb8b5d2b203069f07
eugene dami <eugene.dami@yahoo.fr>
2024-06-04 19:32:16 +0200

    Define general styling and layout

--- script.js
+++ script.js
@@ -49,15 +49,17 @@ const Game = (() => {
   let currentPlayerIndex;
   let otherPlayerIndex;
   let message = document.querySelector("#message");
+  let result = document.querySelector("#result-display");
+  let inputs = document.querySelector("#inputs");
 
   const start = () => {
     players = [
-      createPlayer(document.querySelector("#player1").value || "Player one", "X", 0),
-      createPlayer(document.querySelector("#player2").value || "Player two", "O", 0)
+      createPlayer(document.querySelector("#player1").value || "X", "X", 0),
+      createPlayer(document.querySelector("#player2").value || "O", "O", 0)
     ]
     currentPlayerIndex = 0;
     otherPlayerIndex = 1;
-    gameOver = false;
+    inputs.style.display = "none"
     Gameboard.render();
   }
 
@@ -72,18 +74,17 @@ const Game = (() => {
     let board = Gameboard.boardStatus();
     let mark = players[currentPlayerIndex].mark;
     message.innerText = `${players[otherPlayerIndex].pseudo}'s turn`;
+    result.innerText = "";
     if (board[0] === mark && board[1] === mark && board[2] === mark || board[3] === mark && board[4] === mark && board[5] === mark || board[6] === mark && board[7] === mark && board[8] === mark || board[0] === mark && board[3] === mark && board[6] === mark || board[1] === mark && board[4] === mark && board[7] === mark || board[2] === mark && board[5] === mark && board[8] === mark || board[0] === mark && board[4] === mark && board[8] === mark || board[2] === mark && board[4] === mark && board[6] === mark) {
       players[currentPlayerIndex].score += 1;
-      message.innerHTML += "<br>"
-      message.innerText += `${players[currentPlayerIndex].pseudo} wins, score : ${players[0].pseudo} ${players[0].score} / ${players[1].pseudo} ${players[1].score}`;
+      result.innerText = `${players[currentPlayerIndex].pseudo} wins, score : ${players[0].pseudo} - ${players[0].score} / ${players[1].pseudo} - ${players[1].score}`;
       console.log(board);
     } else if (!board.includes("")) {
-      message.innerHTML += "<br>"
-      message.innerText += `It's a tie, score : ${players[0].pseudo} ${players[0].score} / ${players[1].pseudo} ${players[1].score}`
+      result.innerText = `It's a tie, score : ${players[0].pseudo} - ${players[0].score} / ${players[1].pseudo} - ${players[1].score}`
     }
     currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
     otherPlayerIndex = otherPlayerIndex === 1 ? 0 : 1;
-  })
+  });
 
   return {
     start,
@@ -94,10 +95,10 @@ const Game = (() => {
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
   Game.start();
-})
+});
 
 const restartButton = document.querySelector("#restart-button");
 restartButton.addEventListener("click", () => {
   Gameboard.clear();
   Gameboard.render();
-})
+});
